3da5b1426e90d2b4468623b97b6b0f468795f67b
Jeff Geerling <geerlingguy@mac.com>
2020-06-25 09:51:46 -0500

    Remove erroneously added file.

--- test.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-test = '<script>window["ytInitialData"] = {"responseContext":{"serviceTrackingParams":[{"service":"CSI","params":[{"key":"GetLiveChatReplay_rid","value":"0xb2f8b07452d1f5df"},{"key":"c","value":"WEB"},{"key":"cver","value":"2.20200624.03.00"},{"key":"yt_li","value":"0"}]},"'
-if 'window["s"]' in test:
-    print('FOUNDIT')
